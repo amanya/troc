8807ab28434a403e9a1936e78107e208cd11dfeb
Albert Manyà <albert.manya@adevinta.com>
2024-11-29 17:02:28 +0100

    chapter 6

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +17,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /troc/create", app.trocCreate)
 	mux.HandleFunc("POST /troc/create", app.trocCreatePost)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
 }
